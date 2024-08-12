1fbc589d057c76474e628b1f8b97456dc6ea8e53
sukrijakararic <sukrijakararic1@gmail.com>
2024-08-12 22:59:06 +0200

    added upvotes and changed initial reddit to earthPorn instead of pics

--- src/features/redditPosts/Posts.jsx
+++ src/features/redditPosts/Posts.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { getRedditPosts } from "./redditPostsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./Posts.module.css";
@@ -16,13 +16,30 @@ export const Posts = () => {
     handleFetchPosts();
   }, []);
 
+  const [clickGreen, setClickGreen] = useState('');
+  const [clickRed, setClickRed] = useState('');
+
+  const clickToGreen = () => {
+    setClickGreen('green')
+  }
+
+  const clickToRed = () => {
+    setClickRed('red')
+  }
+
   // if (!posts) return <h1>Loading...</h1>; going to expand on this to have a loading feature
   return (
     <main>
       {posts.map((post) => (
         <article key={post.data.id} className={styles.postContainer}>
-          <h3>{post.data.title}</h3>
+          <div className={styles.topCard}>
+          <div className={styles.upVotes}>
+            <img className={styles.arrow} src="/arrow-up.svg" alt="up-arrow" />
           <p className={styles.ups}>{post.data.ups >= 1000 ? `${(post.data.ups / 1000).toFixed(1)}k` : post.data.ups}</p>
+          <img className={styles.arrow} src="/arrow-down.svg" alt="down-arrow" />
+          </div>
+          <h3>{post.data.title}</h3>
+          </div>
           <img src={post.data.url} />
           <div className="extra-info">
             <span className={styles.time}>{ms(Date.now() - post.data.created_utc * 1000)} ago </span>
