8935240c4b5365f17b7c4007eb7ee0060123600b
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-08-13 20:15:42 +0200

    added svg comonents that change color when clicked.

--- src/features/redditPosts/Posts.jsx
+++ src/features/redditPosts/Posts.jsx
@@ -3,8 +3,8 @@ import { getRedditPosts } from "./redditPostsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./Posts.module.css";
 import ms from "ms";
-
-
+import { ArrowUp } from "../../components/svg-arrows/ArrowUp";
+import { ArrowDown } from "../../components/svg-arrows/ArrowDown";
 
 export const Posts = () => {
   const dispatch = useDispatch();
@@ -18,12 +18,16 @@ export const Posts = () => {
     handleFetchPosts();
   }, []);
 
+  const [green, setGreen] = useState("white");
   const [red, setRed] = useState("white");
 
-  const handleRed = () => {
-    setRed(red === "red" ? "" : "red");
+  const handleGreen = () => {
+    setGreen(green === "green" ? "white" : "green");
   };
 
+  const handleRed = () => {
+    setRed(red === "red" ? "white" : "red");
+  };
   // if (!posts) return <h1>Loading...</h1>; going to expand on this to have a loading feature
   return (
     <main>
@@ -31,17 +35,13 @@ export const Posts = () => {
         <article key={post.data.id} className={styles.postContainer}>
           <div className={styles.topCard}>
             <div className={styles.upVotes}>
-            <img className={styles.arrow} src="/arrow-up.svg" alt="up-arrow" />
+              <ArrowUp fill={green} onClick={handleGreen} />
               <p className={styles.ups}>
                 {post.data.ups >= 1000
                   ? `${(post.data.ups / 1000).toFixed(1)}k`
                   : post.data.ups}
               </p>
-              <img
-                className={styles.arrow}
-                src="/arrow-down.svg"
-                alt="down-arrow"
-              />
+              <ArrowDown fill={red} onClick={handleRed}/>
             </div>
             <h3>{post.data.title}</h3>
           </div>
