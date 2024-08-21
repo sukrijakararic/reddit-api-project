fbb22afd7b5496d424742117147b5854c5cebdbf
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-08-21 17:04:17 +0200

    styles comments and displayed more info from json

--- src/features/comments/CommentsDisplay.jsx
+++ src/features/comments/CommentsDisplay.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styles from "./CommentsDisplay.module.css";
 
+
 export const CommentsDisplay = () => {
   const comments = useSelector((state) => state.redditPosts.comments);
 
   return (
     <div>
       {comments.map((comment) => (
-        <ul>
-          <li key={comment.data.id}>{comment.data.body}</li>
-        </ul>
+        <div className={styles.commentContainer} key={comment.data.id}>
+          <img src={comment.data.author} alt="" />
+          <p className={styles.commentAuthor}>{comment.data.author}</p>     
+          <p key={comment.data.id}>- {comment.data.body}</p>
+        </div>
       ))}
     </div>
   );
